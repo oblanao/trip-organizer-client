491f4e01ff4279ea637ba1f707ac30d2b1521bf9
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-11 00:42:46 +0300

    Updates Profile Layout to consequent style

--- components/Screens/Profile.jsx
+++ components/Screens/Profile.jsx
@@ -1,10 +1,21 @@
-import { Layout, Profile } from '../../components/Demo';
+import { Menu, MenuButton, Tooltip } from '../../components';
+import { Profile } from '../../components/Demo';
 
 const ProfileScreen = () => (
-  <Layout>
-    <h1 className="font-semibold mb-4">My profile</h1>
-    <Profile />
-  </Layout>
+  <div className="font-body text-sm min-h-screen bg-gray-100 flex">
+    <Menu />
+    <main className="max w-full lg:col-span-5 p-4 lg:p-8 xl:px-12 space-y-4">
+      <div className="flex items-center mb-12">
+        <div className="flex flex-1">
+          <h3 className="text-2xl font-semibold">Profile</h3>
+          <Tooltip placement="bottom">Just another tooltip</Tooltip>
+        </div>
+        <MenuButton />
+      </div>
+      <h1 className="font-semibold mb-4">My profile</h1>
+      <Profile />
+    </main>
+  </div>
 );
 
 export default ProfileScreen;
