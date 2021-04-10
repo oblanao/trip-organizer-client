1ffb357689ff257b3ebe75d4c6289b8bc6d71b83
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-10 19:34:58 +0300

    Switches index page to login withoutAuth

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import { checkAuth, withAuth } from '../services/auth';
-import { DashboardScreen } from '../components/Screens';
+import { checkAuth, withoutAuth } from '../services/auth';
+import { LoginScreen } from '../components/Screens';
 
 export async function getServerSideProps(context) {
   return await checkAuth(context);
 }
 
-export default withAuth(DashboardScreen);
+export default withoutAuth(LoginScreen);
