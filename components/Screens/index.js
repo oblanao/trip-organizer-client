c2102c76a78217a644f1a2b323080f77e97ee658
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-10 23:58:10 +0300

    Adds separate ThankYouScreen component

--- components/Screens/index.js
+++ components/Screens/index.js
@@ -2,10 +2,6 @@ import ForgotScreen from './Forgot';
 import DashboardScreen from './Dashboard';
 import LoginScreen from './Login';
 import SignupScreen from './Signup';
+import ThankYouScreen from './ThankYou';
 
-export {
-  ForgotScreen,
-  DashboardScreen,
-  LoginScreen,
-  SignupScreen,
-}
+export { ForgotScreen, DashboardScreen, LoginScreen, SignupScreen, ThankYouScreen };
