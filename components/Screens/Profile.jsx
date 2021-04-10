3df666f081f7b84426f11498410a1bd407411f92
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-11 00:39:42 +0300

    Adds profile component file

---
+++ components/Screens/Profile.jsx
@@ -0,0 +1,10 @@
+import { Layout, Profile } from '../../components/Demo';
+
+const ProfileScreen = () => (
+  <Layout>
+    <h1 className="font-semibold mb-4">My profile</h1>
+    <Profile />
+  </Layout>
+);
+
+export default ProfileScreen;
