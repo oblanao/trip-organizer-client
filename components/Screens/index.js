eb7ec1dcaa9b6e860988221e72fbb93493882e27
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-11 00:35:39 +0300

    Adds separate ProfileScreen component

--- components/Screens/index.js
+++ components/Screens/index.js
@@ -1,7 +1,8 @@
 import ForgotScreen from './Forgot';
 import DashboardScreen from './Dashboard';
 import LoginScreen from './Login';
+import ProfileScreen from './Profile';
 import SignupScreen from './Signup';
 import ThankYouScreen from './ThankYou';
 
-export { ForgotScreen, DashboardScreen, LoginScreen, SignupScreen, ThankYouScreen };
+export { ForgotScreen, DashboardScreen, LoginScreen, ProfileScreen, SignupScreen, ThankYouScreen };
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,15 +1,8 @@
 import { checkAuth, withAuth } from '../../services/auth';
-import { Layout, Profile } from '../../components/Demo';
-
-const Page = () => (
-  <Layout>
-    <h1 className="font-semibold mb-4">My profile</h1>
-    <Profile />
-  </Layout>
-);
+import { ProfileScreen } from '../../components/Screens';
 
 export async function getServerSideProps(context) {
   return await checkAuth(context);
 }
 
-export default withAuth(Page);
+export default withAuth(ProfileScreen);
