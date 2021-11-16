cc204b3c65d008a018513156ac6e538e211c2fac
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-11-16 19:20:44 +0200

    Shows role in user profile

--- components/Demo/ProfileLoading.jsx
+++ components/Demo/ProfileLoading.jsx
@@ -10,6 +10,10 @@ const ProfileLoading = () => (
       <dt className="lg:w-1/4">Email: </dt>
       <dd className="h-4 my-1 bg-gray-300 rounded-lg w-1/2"></dd>
     </div>
+    <div className="flex flex-col px-4 lg:px-8 bg-white py-2">
+      <dt className="lg:w-1/4">Role: </dt>
+      <dd className="h-4 my-1 bg-gray-300 rounded-lg w-1/2"></dd>
+    </div>
   </div>
 );
 
--- components/Demo/ProfileSuccess.jsx
+++ components/Demo/ProfileSuccess.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProfileSuccess = ({ name, email }) => (
+const ProfileSuccess = ({ name, email, role }) => (
   <>
     <div className="flex flex-col px-4 lg:px-8 bg-white py-2">
       <dt className="lg:w-1/4">Name: </dt>
@@ -9,6 +9,10 @@ const ProfileSuccess = ({ name, email }) => (
     <div className="flex flex-col px-4 lg:px-8 bg-white py-2">
       <dt className="lg:w-1/4">Email: </dt>
       <dd className="font-semibold">{email}</dd>
+    </div>
+    <div className="flex flex-col px-4 lg:px-8 bg-white py-2">
+      <dt className="lg:w-1/4">Role: </dt>
+      <dd className="font-semibold">{role}</dd>
     </div>
   </>
 );
